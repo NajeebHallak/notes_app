2ab3a3dad313271fe8016dbc2c7b0c3a14e06ba3
Your Name <your_email@example.com>
2024-10-24 23:58:03 +0300

    add hive is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/views/edit_notes_view.dart';
 import 'package:notes_app/views/notes_app_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  var box = Hive.box('myBox');
   runApp(const NptesApp());
 }
 
